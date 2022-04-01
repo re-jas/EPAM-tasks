8149fe2c890753e1a7c887ab8fdf61e25f15ce23
Jasur Erkinov <re.nine.jas@gmail.com>
2022-04-01 21:57:21 +0500

    made a correction in task

--- 8. Exception handling/1.js
+++ 8. Exception handling/1.js
@@ -1,22 +1,21 @@
 // In a try catch construction, wrap the code: console.log (a), let a = 3.
-// And display an error - 'let must be declared' before use.
+// And display an error - ‘let must be declared’ before use.
 
 try {
   console.log(a);
   let a = 3;
 } catch (e) {
-  if (e.name == "ReferenceError") {
-    console.log("let must be declared");
-  }
+  console.log("let must be declared");
 }
 
 // When running 1/0, the error 'cannot be divided by zero'
 
 try {
-  let a = 1 / 0;
-  console.log(a);
-} catch (e) {
-  if (e.name == "Infinity") {
-    console.log("cannot be divided by zero");
+  let a = 1,
+    b = 0;
+  if (a / b === Infinity) {
+    throw "cannot be divided by zero";
   }
+} catch (e) {
+  console.log(e);
 }
